0c8c92ce784360bbc652315cb13d9c3f2d13a7be
DylanToporek <dylantoporek@gmail.com>
2021-10-31 15:47:39 -0400

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
+  const [questions, setQuestions] = useState([])
+
+  function handleQuestions(question){
+    setQuestions(question)
+  }
+
+  function addNewQuestion(question){
+    setQuestions(...questions, question)
+  }
+
+  console.log(questions)
 
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? <QuestionForm addQuestion={addNewQuestion} setPage={setPage}/> : <QuestionList questions={questions} getQuestions={handleQuestions} page={page}/>}
     </main>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import QuestionItem from "./QuestionItem";
 
-function QuestionList() {
+function QuestionList({getQuestions, questions, page}) {
+  
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+    .then(res => res.json())
+    .then(data => getQuestions(data))
+
+    return function cleanup(){
+      console.log("done")
+    }
+  }, [page])
+
+  const questionsDisplay = questions.map((question) => {
+    return <QuestionItem key={question.id} question={question}/>
+  })
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>
+        {questionsDisplay}
+      </ul>
     </section>
   );
 }
